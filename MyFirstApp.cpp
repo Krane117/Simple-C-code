deadb7e30022a20f8959105f784f9f6ebfac2ae3
AndrewUrus117 <93611789+Krane117@users.noreply.github.com>
2024-02-06 22:51:31 +0100

    Add files via upload

---
+++ MyFirstApp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std; //for reductions cout text
+
+int main()
+{
+
+    cout << "Hello World!\n";
+    cout << "My name is Andrew. "<<endl;
+    cout << "Today is Monday";
+
+    system("pause>0");
+}
